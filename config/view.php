b0a62a07236556ffefa5f8fefcdb706ef6df505d
Bayu <bayusatriowid@gmail.com>
2023-05-15 16:10:18 +0700

    test

--- config/view.php
+++ config/view.php
@@ -7,7 +7,7 @@ return [
     | View Storage Paths
     |--------------------------------------------------------------------------
     |
-    | Most templating systems load templates from disk. Here you may specify
+    | Mosta templating systems load templates from disk. Here you may specify
     | an array of paths that should be checked for your views. Of course
     | the usual Laravel view path has already been registered for you.
     |
